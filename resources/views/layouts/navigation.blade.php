15a8dd66cb8693b18c97e52e422ef79b5546fcb1
RafiMFikri <rafimfiqri@gmail.com>
2023-12-04 20:02:44 +0700

    Laravel Breeze & Spatie Laravel

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,18 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('book')" :active="request()->routeIs('book.create')">
+                        {{ __('Book') }}
+                    </x-nav-link>
+                </div>
+                @hasrole('pustakawan')
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('role')" :active="request()->routeIs('role')">
+                        {{ __('Roles') }}
+                    </x-nav-link>
+                </div>
+                @endhasrole
             </div>
 
             <!-- Settings Dropdown -->
